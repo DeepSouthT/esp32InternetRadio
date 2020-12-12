30b6adf8fd192d2144b6559d9cfa60acd225c39d
DeepSouthT <deepakcheriyan@yahoo.com>
2020-12-12 21:32:49 +0100

    oled display

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,17 @@
 #include "wifihelper.h"
 #include "sdhelper.h"
 
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 #define VS1053_CS 32
 #define VS1053_DCS 33
 #define VS1053_DREQ 35
@@ -47,6 +58,17 @@ void initMP3Decoder()
   player.setVolume(VOLUME);
 }
 
+void displayText(String text) {
+  display.clearDisplay();
+
+  display.setTextSize(2); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println(F(text.c_str()));
+  display.display();      // Show initial text
+  delay(100);
+}
+
 void setup()
 {
 
@@ -57,6 +79,19 @@ void setup()
   Serial.println("--------------------------");
 
   SPI.begin();
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(1000); // Pause for 1 seconds
+
+  displayText("Reading SD");
 
   // ----- Get the SSID and PSW -----
   Serial.println("Reading SD card");
@@ -77,13 +112,14 @@ void setup()
   }
   // --------------------------------
 
+  displayText("Reading station");
   // ------- Read station list ------
   uint8_t ret = objSdHelper.readStationList();
   Serial.printf("Available stations: %d\n", ret);
 
   if (status && (ret > 0))
   {
-    status = objSdHelper.getStation(3, play_station);
+    status = objSdHelper.getStation(0, play_station);
   }
   if (!status)
   {
@@ -99,6 +135,7 @@ void setup()
 
   initMP3Decoder();
 
+  displayText("Connect WiFi");
   // ----- Connecting to WiFi -------
   Serial.println("Connecting to WiFi");
 
@@ -118,6 +155,7 @@ void setup()
   }
   // --------------------------------
 
+  displayText("Connect Client");
   // ----- Connecting to Station/Client -------
   if (status)
   {
@@ -132,6 +170,8 @@ void setup()
     Serial.println("Client not connected");
   }
   // --------------------------------
+
+  displayText("Streaming" + play_station.name);
 }
 
 void loop()
